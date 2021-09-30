575a3095a1a1869e31154ef05fa655490acc5198
Danish A. Alvi <danishamjad@gmail.com>
2021-09-30 20:39:18 +0500

    Add files via upload
    A Haskell implementation of Bron-Kerbosch algorithm to find maximal cliques in a graph.

---
+++ bron-kerbosch.hs
@@ -0,0 +1,18 @@
+import Data.List
+import Data.Ord
+
+type Graph = [(Int, [Int])]
+
+adjacentVertices :: Graph -> (Int, [Int]) -> Graph
+adjacentVertices [] _ = []
+adjacentVertices g v = [x | x <- g, (fst v) `elem` (snd x)]
+
+maxCliques :: Graph -> Graph -> Graph -> [Graph] -> [Graph]
+maxCliques r [] [] s = r:s
+maxCliques _ [] _ s = s
+maxCliques r (p:ps) x s = (maxCliques r ps (p:x) (maxCliques (p:r) (adjacentVertices ps p) (adjacentVertices x p) s))
+
+showClique [] = []
+showClique s = sort [fst x | x <- s]
+
+functionCliques g = [showClique s | s <- (maxCliques [] g [] [])]
